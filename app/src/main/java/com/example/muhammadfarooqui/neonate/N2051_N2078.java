b0ff7513cd8e2d311ab614ac0a36484f3e79edf6
ali.azaz <ali.azaz.alam@hotmail.com>
2018-08-11 18:53:36 +0500

    layout and skip pattern done n2023__n2026,n2051__n2078 and n2251__n2260.xml

--- app/src/main/java/com/example/muhammadfarooqui/neonate/N2051_N2078.java
+++ app/src/main/java/com/example/muhammadfarooqui/neonate/N2051_N2078.java
@@ -1,13 +1,52 @@
 package com.example.muhammadfarooqui.neonate;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
+
+import com.example.muhammadfarooqui.neonate.databinding.N2051N2078Binding;
+
+import utils.ClearAllcontrol;
 
 public class N2051_N2078 extends AppCompatActivity {
+
+    N2051N2078Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.n2051__n2078);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.n2051__n2078);
+        bi.setCallback(this);
+
+        SetContentUI();
+
+    }
+
+    public void SetContentUI() {
+
+        bi.rgN2271.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.rbN22711.getId()) {
+                    ClearAllcontrol.ClearAll(bi.llN2272N2278);
+                }
+            }
+        });
+
+        bi.rgN2272.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == bi.rbN22722.getId()) {
+                    ClearAllcontrol.ClearAll(bi.llN2272N2278);
+                }
+            }
+        });
+
+    }
+
+    public void BtnContinue() {
+
     }
 }
--- app/src/main/java/com/example/muhammadfarooqui/neonate/N2251_N2260.java
+++ app/src/main/java/com/example/muhammadfarooqui/neonate/N2251_N2260.java
@@ -1,13 +1,53 @@
 package com.example.muhammadfarooqui.neonate;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
+
+import com.example.muhammadfarooqui.neonate.databinding.N2251N2260Binding;
+
+import utils.ClearAllcontrol;
 
 public class N2251_N2260 extends AppCompatActivity {
+
+
+    N2251N2260Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.n2251__n2260);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.n2251__n2260);
+        bi.setCallback(this);
+
+        SetContentUI();
+
+    }
+
+    public void SetContentUI() {
+
+        bi.rgN2253.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.rbN22531.getId()) {
+                    ClearAllcontrol.ClearAll(bi.llN2254N2258);
+                }
+            }
+        });
+
+        bi.rgN2254.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == bi.rbN22542.getId()) {
+                    ClearAllcontrol.ClearAll(bi.llN2255N2258);
+                }
+            }
+        });
+
+    }
+
+    public void BtnContinue() {
+
     }
 }
