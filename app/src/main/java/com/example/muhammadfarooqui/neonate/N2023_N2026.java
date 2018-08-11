20cd683a6102ada54d58d05370e787f8ba2e7bf6
ali.azaz <ali.azaz.alam@hotmail.com>
2018-08-11 13:10:27 +0500

    some changes and done layout n2023_n2026

--- app/src/main/java/com/example/muhammadfarooqui/neonate/N2023_N2026.java
+++ app/src/main/java/com/example/muhammadfarooqui/neonate/N2023_N2026.java
@@ -1,13 +1,25 @@
 package com.example.muhammadfarooqui.neonate;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.muhammadfarooqui.neonate.databinding.N2023N2026Binding;
+
 public class N2023_N2026 extends AppCompatActivity {
+
+    N2023N2026Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.n2023__n2026);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.n2023__n2026);
+        bi.setCallback(this);
+
+    }
+
+    public void BtnContinue() {
+
     }
 }
