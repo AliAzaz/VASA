97ca22872397865c60c9b6af5256b0eb7e3c5f67
ali.azaz <ali.azaz.alam@hotmail.com>
2018-08-15 15:19:40 +0500

    re-build previous layouts done

--- app/src/main/java/com/example/muhammadfarooqui/neonate/N2017_N2022_3.java
+++ app/src/main/java/com/example/muhammadfarooqui/neonate/N2017_N2022_3.java
@@ -1,13 +1,26 @@
 package com.example.muhammadfarooqui.neonate;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.muhammadfarooqui.neonate.databinding.N2017N20223Binding;
+
 public class N2017_N2022_3 extends AppCompatActivity {
+
+    N2017N20223Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.n2017__n2022_3);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.n2017__n2022_3);
+        bi.setCallback(this);
+
+    }
+
+    public void BtnContinue() {
+        startActivity(new Intent(this, N2023_N2026.class));
     }
 }
