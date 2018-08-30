d91250fd612e07fe1e59b59c84adc945371a85f0
ali.azaz <ali.azaz.alam@hotmail.com>
2018-08-30 18:09:09 +0500

    COMMIT

--- app/src/main/java/com/example/muhammadfarooqui/neonate/N2001_N2011.java
+++ app/src/main/java/com/example/muhammadfarooqui/neonate/N2001_N2011.java
@@ -97,7 +97,12 @@ public class N2001_N2011 extends AppCompatActivity {
 
     public void BtnContinue() {
         if (validateField()) {
-            startActivity(new Intent(this, N2012_N2016.class));
+            if (SaveData()) {
+                startActivity(new Intent(this, N2012_N2016.class));
+            } else {
+                Toast.makeText(this, "Can't add data!!", Toast.LENGTH_SHORT).show();
+            }
+
         } else {
             Toast.makeText(this, "Required fields are missing", Toast.LENGTH_SHORT).show();
         }
@@ -107,38 +112,44 @@ public class N2001_N2011 extends AppCompatActivity {
 
         Global.N.N2001_N2011 n2001 = new Global.N.N2001_N2011();
 
-        Global.N.N2001_N2011.setN2001(bi.rbN20011.isChecked() ? "1" : bi.rbN20012.isChecked() ? "2" : bi.rbN2001DK.isChecked() ? "9"
+        n2001.setN2001(bi.rbN20011.isChecked() ? "1" : bi.rbN20012.isChecked() ? "2" : bi.rbN2001DK.isChecked() ? "9"
                 : bi.rbN2001RA.isChecked() ? "8" : "");
-        Global.N.N2001_N2011.setN2002(bi.rbN20021.isChecked() ? "1" : bi.rbN20022.isChecked() ? "2" : bi.rbN2002DK.isChecked() ? "9"
+        n2001.setN2002(bi.rbN20021.isChecked() ? "1" : bi.rbN20022.isChecked() ? "2" : bi.rbN2002DK.isChecked() ? "9"
                 : bi.rbN2002RA.isChecked() ? "8" : "");
-        Global.N.N2001_N2011.setN2003(bi.rbN20031.isChecked() ? "1" : bi.rbN20032.isChecked() ? "2" : bi.rbN2003DK.isChecked() ? "9"
+        n2001.setN2003(bi.rbN20031.isChecked() ? "1" : bi.rbN20032.isChecked() ? "2" : bi.rbN2003DK.isChecked() ? "9"
                 : bi.rbN2003RA.isChecked() ? "8" : "");
-        Global.N.N2001_N2011.setN2004(bi.rbN20041.isChecked() ? "1" : bi.rbN20042.isChecked() ? "2" : bi.rbN20043.isChecked() ? "3" : bi.rbN2004DK.isChecked() ? "9"
+        n2001.setN2004(bi.rbN20041.isChecked() ? "1" : bi.rbN20042.isChecked() ? "2" : bi.rbN20043.isChecked() ? "3" : bi.rbN2004DK.isChecked() ? "9"
                 : bi.rbN2004RA.isChecked() ? "8" : "");
-        Global.N.N2001_N2011.setN2005u(bi.rbN2005u1.isChecked() ? "1" : bi.rbN2005u2.isChecked() ? "2" : bi.rbN2005u3.isChecked() ? "3" : bi.rbN2005uDK.isChecked() ? "9"
+        n2001.setN2005u(bi.rbN2005u1.isChecked() ? "1" : bi.rbN2005u2.isChecked() ? "2" : bi.rbN2005u3.isChecked() ? "3" : bi.rbN2005uDK.isChecked() ? "9"
                 : bi.rbN2005uRA.isChecked() ? "8" : "");
-        Global.N.N2001_N2011.setN2005d(bi.edN2005d.getText().toString());
-        Global.N.N2001_N2011.setN2005w(bi.edN2005w.getText().toString());
-        Global.N.N2001_N2011.setN2005m(bi.edN2005m.getText().toString());
-        Global.N.N2001_N2011.setN2006(
+        n2001.setN2005d(bi.edN2005d.getText().toString());
+        n2001.setN2005w(bi.edN2005w.getText().toString());
+        n2001.setN2005m(bi.edN2005m.getText().toString());
+        n2001.setN2006(
                 bi.rbN20061.isChecked() ? "1" : bi.rbN20062.isChecked() ? "2" : bi.rbN20063.isChecked() ? "3" :
                         bi.rbN20064.isChecked() ? "4" : bi.rbN20065.isChecked() ? "5" : bi.rbN20066.isChecked() ? "6" :
                                 bi.rbN20067.isChecked() ? "7" : bi.rbN20068.isChecked() ? "8" : bi.rbN20069.isChecked() ? "9" :
                                         bi.rbN200610.isChecked() ? "10" : bi.rbN200611.isChecked() ? "11" : bi.rbN200612.isChecked() ? "12"
-                                                : bi.rbN2005uDK.isChecked() ? "99" : bi.rbN2005uRA.isChecked() ? "88" : "");
-        /*n2001.setN2008();
-        n2001.setN20091();
-        n2001.setN20092();
-        n2001.setN20093();
-        n2001.setN20094();
-        n2001.setN2010();
-        n2001.setN2011();*/
+                                                : bi.rbN2006DK.isChecked() ? "99" : bi.rbN2006RA.isChecked() ? "88" : "");
+        n2001.setN2008(bi.rbN20081.isChecked() ? "1" : bi.rbN20082.isChecked() ? "2" : bi.rbN20083.isChecked() ? "3" :
+                bi.rbN20084.isChecked() ? "4" : bi.rbN20085.isChecked() ? "5" : bi.rbN20086.isChecked() ? "6"
+                        : bi.rbN2008OT.isChecked() ? "7" : bi.rbN20088.isChecked() ? "8" : bi.rbN2008DK.isChecked() ? "9" : "");
+        n2001.setN20091(bi.rbN200911.isChecked() ? "1" : bi.rbN200912.isChecked() ? "2" : bi.rbN20091DK.isChecked() ? "9"
+                : bi.rbN20091RA.isChecked() ? "8" : "");
+        n2001.setN20092(bi.rbN200921.isChecked() ? "1" : bi.rbN200922.isChecked() ? "2" : bi.rbN20092DK.isChecked() ? "9"
+                : bi.rbN20092RA.isChecked() ? "8" : "");
+        n2001.setN20093(bi.rbN200931.isChecked() ? "1" : bi.rbN200932.isChecked() ? "2" : bi.rbN20093DK.isChecked() ? "9"
+                : bi.rbN20093RA.isChecked() ? "8" : "");
+        n2001.setN20094(bi.rbN200941.isChecked() ? "1" : bi.rbN200942.isChecked() ? "2" : bi.rbN20094DK.isChecked() ? "9"
+                : bi.rbN20094RA.isChecked() ? "8" : "");
+        n2001.setN2010(bi.edN2010.getText().toString());
+        n2001.setN2011(bi.rbN20111.isChecked() ? "1" : bi.rbN20112.isChecked() ? "2" : "");
 
 
         DBHelper db = new DBHelper(this);
-        db.add_N2001(n2001);
+        Long row = db.add_N2001(n2001, this);
 
-        return true;
+        return row > 0;
     }
 
     public Boolean validateField() {
@@ -172,7 +183,7 @@ public class N2001_N2011 extends AppCompatActivity {
                     return false;
                 }
 
-                if (!bi.rbN2005uDK.isChecked() || !bi.rbN2005uRA.isChecked()) {
+                if (bi.rbN2005u1.isChecked()) {
                     //ll_N2005d
                     if (!Gothrough.IamHiden(bi.llN2005d)) {
                         return false;
@@ -180,7 +191,9 @@ public class N2001_N2011 extends AppCompatActivity {
                     if (validations.RangeTextBox(this, bi.edN2005d, 0, 6, "Days")) {
                         return false;
                     }
+                }
 
+                if (bi.rbN2005u2.isChecked()) {
                     //ll_N2005w
                     if (!Gothrough.IamHiden(bi.llN2005w)) {
                         return false;
@@ -189,6 +202,9 @@ public class N2001_N2011 extends AppCompatActivity {
                         return false;
                     }
 
+                }
+
+                if (bi.rbN2005u3.isChecked()) {
                     //ll_N2005m
                     if (!Gothrough.IamHiden(bi.llN2005m)) {
                         return false;
--- app/src/main/java/data/N/N2001_N2011.java
+++ app/src/main/java/data/N/N2001_N2011.java
@@ -7,22 +7,22 @@ public class N2001_N2011 {
         String query;
         query = "CREATE TABLE '" + TABLE_NAME + "' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'study_id' INTEGER,   " +
 
-                Global.N.N2001_N2011.N2001 + " TEXT," +
-                Global.N.N2001_N2011.N2002 + " TEXT," +
-                Global.N.N2001_N2011.N2003 + " TEXT," +
-                Global.N.N2001_N2011.N2004 + " TEXT," +
-                Global.N.N2001_N2011.N2005u + " TEXT," +
-                Global.N.N2001_N2011.N2005d + " TEXT," +
-                Global.N.N2001_N2011.N2005w + " TEXT," +
-                Global.N.N2001_N2011.N2005m + " TEXT," +
-                Global.N.N2001_N2011.N2006 + " TEXT," +
-                Global.N.N2001_N2011.N2008 + " TEXT," +
-                Global.N.N2001_N2011.N2009_1 + " TEXT," +
-                Global.N.N2001_N2011.N2009_2 + " TEXT," +
-                Global.N.N2001_N2011.N2009_3 + " TEXT," +
-                Global.N.N2001_N2011.N2009_4 + " TEXT," +
-                Global.N.N2001_N2011.N2010 + " TEXT," +
-                Global.N.N2001_N2011.N2011 + " TEXT" +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2001 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2002 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2003 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2004 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2005u + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2005d + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2005w + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2005m + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2006 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2008 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2009_1 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2009_2 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2009_3 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2009_4 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2010 + " TEXT," +
+                Global.N.N2001_N2011.sub_N2001_N2011.N2011 + " TEXT" +
                 ')';
 
 
