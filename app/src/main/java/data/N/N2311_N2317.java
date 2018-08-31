d7344a6093d3c6f65bb55c8f79b343f5ee37d1cf
ali.azaz <ali.azaz.alam@hotmail.com>
2018-08-31 20:44:56 +0500

    save draft contd and modified model classe

--- app/src/main/java/data/N/N2311_N2317.java
+++ app/src/main/java/data/N/N2311_N2317.java
@@ -6,15 +6,50 @@ public class N2311_N2317 {
 
     public static String getCreateQuery() {
         String query;
-        query = "CREATE TABLE '" + TABLE_NAME + "' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'study_id' INTEGER,   " +
+        query = "CREATE TABLE '" + TABLE_NAME + "' ('id' INTEGER PRIMARY KEY AUTOINCREMENT,   " +
 
-                Global.N.N2311_N2317.N2311 + " TEXT," +
-                Global.N.N2311_N2317.N2312 + " TEXT," +
-                Global.N.N2311_N2317.N2313 + " TEXT," +
-                Global.N.N2311_N2317.N2314 + " TEXT," +
-                Global.N.N2311_N2317.N2315 + " TEXT," +
-                Global.N.N2311_N2317.N2316 + " TEXT," +
-                Global.N.N2311_N2317.N2317 + " TEXT" +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_1 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_2 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_3 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_4 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_5 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_6 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_7 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_8 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_9 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_10 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_11 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_12 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2311_13 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_1 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_2 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_3 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_4 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_5 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_6 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_7 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_8 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2312_9 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_1 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_2 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_3 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_4 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_5 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_6 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_7 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_8 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_9 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_10 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_11 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2313_12 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2314 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2315 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2316 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.N2317 + " TEXT," +
+                Global.N.N2311_N2317.sub_N2311_N2317.STUDYID + " INTEGER" +
 
                 ')';
 
